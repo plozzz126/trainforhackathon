68e34cbefd1343e6f6f93d11f409d83fd7e2620c
plozzz126 <rpan999s@gmail.com>
2026-03-15 20:37:05 +0600

    Первый HTTP сервер на чистом Go

--- 1and2Days/firstday1a2/firstday.go
+++ 1and2Days/firstday1a2/firstday.go
@@ -1,17 +1,45 @@
-package first1
+package main
+
 import (
 	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Работает?"))
-	
+type SumReq struct {
+	A int `json:"a"`
+	B int `json:"b"`
 }
 
+type SumRes struct {
+	Result int `json:"result"`
+}
 
-func first1(){
-	http.HandleFunc("/test", handler)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{
+		"Hl": "Hello World",
+	}
+	json.NewEncoder(w).Encode(resp)
+}
+
+func sumHandler(w http.ResponseWriter, r *http.Request) {
+
+	var req SumReq
+	json.NewDecoder(r.Body).Decode(&req)
+
+	result := req.A + req.B
+
+	resp := SumRes{
+		Result: result,
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/sum", sumHandler)
+	fmt.Println("Server started on port 8080")
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("прост что бы не ругало")
 }
